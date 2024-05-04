c82d69abd676905461f228c24af896555860c997
dzani <dzanieterle531@gmail.com>
2024-05-04 16:54:07 +0200

    Added friendlist to user class to coinscide with another branch

--- Meet-Friends-BE/src/main/java/com/MeetFriendsBE/User.java
+++ Meet-Friends-BE/src/main/java/com/MeetFriendsBE/User.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,5 +14,12 @@ public class User {
     private long id;
     private String Username;
     private String Password;
+    private List<User> friendslist;
+
+    public User(long id, String username, String password) {
+        this.id=id;
+        this.Username=username;
+        this.Password=password;
+    }
 
 }
