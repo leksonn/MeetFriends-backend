3729a73cee2f23cf9461d14c3bae1949beec75a0
Dzani-Eterle <127309947+Dzani-Eterle@users.noreply.github.com>
2024-05-04 16:11:52 +0200

    Added project files again
    This time it is a blank slate without any prior work

---
+++ Meet-Friends-BE/src/main/java/com/MeetFriendsBE/MeetFriendsBeApplication.java
@@ -0,0 +1,13 @@
+package com.MeetFriendsBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetFriendsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetFriendsBeApplication.class, args);
+	}
+
+}
